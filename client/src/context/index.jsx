20e867ec22f61c13f9dc440f9df764609808390c
Mahad <mahad9036@gmail.com>
2024-05-07 21:43:07 +0500

    "minor modifications"

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -2,7 +2,7 @@ import React, { useContext, createContext } from 'react';
 
 import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
 import { ethers } from 'ethers';
-import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
+// import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 
 const StateContext = createContext();
 
