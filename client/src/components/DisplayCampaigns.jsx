40a2314d14f1f096e7379b70e1a9da082ec1c4f6
Mahad <mahad9036@gmail.com>
2024-05-27 10:53:35 +0500

    Filtered campaigns

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -12,30 +12,36 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
         navigate(`/campaign-details/${campaign.title}`, { state: campaign })
     }
 
+    // Filter campaigns with remaining days >= 0
+    const filteredCampaigns = campaigns.filter(campaign => daysLeft(campaign.deadline) >= 0);
+
     return (
         <div>
-        <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">{title} ({campaigns.length})</h1>
+            <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">
+                {title} ({filteredCampaigns.length})
+            </h1>
 
-        <div className="flex flex-wrap mt-[20px] gap-[26px]">
-            {isLoading && (
-            <img src={loader} alt="loader" className="w-[100px] h-[100px] object-contain" />
-            )}
+            <div className="flex flex-wrap mt-[20px] gap-[26px]">
+                {isLoading && (
+                    <img src={loader} alt="loader" className="w-[100px] h-[100px] object-contain" />
+                )}
 
-            {!isLoading && campaigns.length === 0 && (
-            <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
-                You have not created any campigns yet
-            </p>
-            )}
+                {!isLoading && filteredCampaigns.length === 0 && (
+                    <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
+                        You have not created any campaigns yet
+                    </p>
+                )}
 
-            {!isLoading && campaigns.length > 0 && campaigns.map((campaign) => <FundCard 
-            key={uuidv4()}
-            {...campaign}
-            handleClick={() => handleNavigate(campaign)}
-            />)}
+                {!isLoading && filteredCampaigns.length > 0 && filteredCampaigns.map((campaign) => (
+                    <FundCard 
+                        key={uuidv4()}
+                        {...campaign}
+                        handleClick={() => handleNavigate(campaign)}
+                    />
+                ))}
+            </div>
         </div>
-    </div>
-)
+    )
 }
 
-export default DisplayCampaigns
-
+export default DisplayCampaigns;
