fd712e89ed8e2581aca5ef28a5626fd2f68970ba
Mahad <mahad9036@gmail.com>
2024-06-04 19:10:53 +0500

    Add navbar details

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,4 @@
-export { default as Sidebar } from './Sidebar';
-export { default as Navbar } from './Navbar';
-export { default as AdminSidebar} from './AdminSidebar'
+export { default as AdminNavbar } from './AdminNavbar';
 export { default as CustomButton } from './CustomButton'
 export { default as FormField } from './FormField';
 export { default as DisplayCampaigns} from './DisplayCampaigns'
@@ -11,3 +9,4 @@ export { default as CountBox } from './CountBox'
 export { default as SubmitRequest } from './SubmitRequest'
 export { default as RequestCard } from './RequestCard'
 export { default as DisplayAllCampaigns } from './DisplayAllCampaigns'
+export { default as UserNavbar } from './UserNavbar'
